4bfaf6c7694a69a888670615be234ade882f89d6
vecseizsolt <vecseizs@kkszki.hu>
2024-10-18 10:29:14 +0200

    A dolgozat első feladata elkészült és sikeresen működik.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat_10_18_01_feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. feladat
+        }
+    }
+}
